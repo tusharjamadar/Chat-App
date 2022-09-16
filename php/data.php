137d4f85123bab8cb32a588246bd4af3a32ce3dc
Tushar Jamadar <tusharjamadar2001@gmail.com>
2022-09-16 10:01:35 +0530

    Small modifications

--- php/data.php
+++ php/data.php
@@ -6,13 +6,17 @@
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
         (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
+
         (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
+
         if(isset($row2['outgoing_msg_id'])){
             ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
         }else{
             $you = "";
         }
+
         ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+        
         ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
 
         $output .= '<a href="chat.php?user_id='. $row['unique_id'] .'">
--- php/search.php
+++ php/search.php
@@ -6,6 +6,7 @@
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
 
     $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
+    
     $output = "";
     $query = mysqli_query($conn, $sql);
     if(mysqli_num_rows($query) > 0){
